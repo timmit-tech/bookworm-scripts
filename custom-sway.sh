b66257bef60df50ded291035fcd33fee34142b2e
Rickard Timm <rickard.timm@.liu.se>
2024-05-18 19:05:22 +0200

    Add config for sway

---
+++ custom-sway.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Config from lern linux tv
+#
+# Sway config
+#
+cp ./config/sway/config ~/.config/sway/config
+cp ./config/sway/audo.sh ~/.config/sway/audio.sh
+cp ./config/sway/exit.sh ~/.config/sway/exit.sh
+cp ./config/sway/lock_screen.sh ~/.config/sway/lock_screen.sh
+#
+# waybar config
+#
+cp ./config/waybar/config ~/.config/waybar/config
+cp ./config/waybar/style.css ~/.config/waybar/style.css
+#
+# wofi config
+#
+cp ./config/wofi/style.css ~/.config/wofi/style.css
+#
+# Script need to be executable
+#
+chmod +x ~/.config/sway/*.sh
+#
+# copy wallpaper
+#
+cp ./config/wallpaper.jpg ~/.config/sway/wallpaper.jpg
